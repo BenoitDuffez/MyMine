ca3556efaf82a947fd065d55a042cc6d17fa38ff
Benoit Duffez <benoit@bicou.net>
2013-06-25 18:53:47 +0200

    improved monkey script

--- tools/monkey.sh
+++ tools/monkey.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cmd="adb shell monkey -v -v -p net.bicou.redmine 50000 -s `date +%s` --pct-syskeys 0"
+cmd="adb shell monkey -v -v -p net.bicou.redmine -s `date +%s` --pct-syskeys 0 50000"
 echo "trying: $cmd"
 $cmd > .monkey.log &
 adb logcat -v time | grep BicouRedmine
