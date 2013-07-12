2d2d832fb87f7b4c45b5f32eb116cda4c5eaece6
Benoit Duffez <benoit@bicou.net>
2013-07-12 10:42:31 +0200

    refs #289 @10m

--- src/net/bicou/redmine/data/json/UsersList.java
+++ src/net/bicou/redmine/data/json/UsersList.java
@@ -1,7 +1,26 @@
 package net.bicou.redmine.data.json;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class UsersList {
+public class UsersList extends AbsObjectList<User> {
 	public List<User> users;
+
+	@Override
+	public void addObjects(List<User> objects) {
+		if (users == null) {
+			users = new ArrayList<User>();
+		}
+		users.addAll(objects);
+	}
+
+	@Override
+	public List<User> getObjects() {
+		return users;
+	}
+
+	@Override
+	public int getSize() {
+		return users == null ? 0 : users.size();
+	}
 }
