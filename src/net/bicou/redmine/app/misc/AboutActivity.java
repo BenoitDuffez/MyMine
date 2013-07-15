32eefe55dbf2dd63b588cc48bc37b3049dc1897d
Benoit Duffez <benoit@bicou.net>
2013-07-15 18:09:25 +0200

    fixes #286 @20m

--- src/net/bicou/redmine/app/misc/AboutActivity.java
+++ src/net/bicou/redmine/app/misc/AboutActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
+import net.bicou.redmine.R;
 
 /**
  * Created by bicou on 17/06/13.
@@ -12,8 +13,9 @@ public class AboutActivity extends SherlockFragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new AboutFragment()).commit();
+			getSupportFragmentManager().beginTransaction().add(R.id.about_content, new AboutFragment()).commit();
 		}
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
