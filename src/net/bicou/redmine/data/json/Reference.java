1e21e3dd184faacd91c165036fd0ddb8f3c6acd3
Benoit Duffez <benoit@bicou.net>
2013-07-18 16:57:45 +0200

    added Cursor constructor

--- src/net/bicou/redmine/data/json/Reference.java
+++ src/net/bicou/redmine/data/json/Reference.java
@@ -1,14 +1,26 @@
 package net.bicou.redmine.data.json;
 
 
+import android.database.Cursor;
+import net.bicou.redmine.util.L;
+
 public class Reference {
 	public String name;
 	public long id;
 
 	public static final String KEY_NAME = "name";
+	public static final String KEY_ID = "id";
 
 	public Reference() {
+	}
 
+	public Reference(Cursor c) {
+		try {
+			name = c.getString(c.getColumnIndex(KEY_NAME));
+			id = c.getLong(c.getColumnIndex(KEY_ID));
+		} catch (Exception e) {
+			L.e("Couldn't get name/id from Reference", e);
+		}
 	}
 
 	@Override
