c1dba44cfd37cc2045279088807864f6965ffcd1
Benoit Duffez <benoit@bicou.net>
2013-07-18 16:58:04 +0200

    bug fix in issue status creation from a cursor

--- src/net/bicou/redmine/data/json/IssueStatus.java
+++ src/net/bicou/redmine/data/json/IssueStatus.java
@@ -1,26 +1,24 @@
 package net.bicou.redmine.data.json;
 
+import android.database.Cursor;
 import net.bicou.redmine.data.sqlite.DbAdapter;
 import net.bicou.redmine.data.sqlite.IssueStatusesDbAdapter;
 import net.bicou.redmine.util.L;
-import android.database.Cursor;
 
 public class IssueStatus extends Reference {
 	public boolean is_default;
 	public boolean is_closed;
 
 	public IssueStatus(final Cursor c, final DbAdapter db) {
+		super(c);
+
 		for (final String col : IssueStatusesDbAdapter.ISSUE_STATUS_FIELDS) {
 			try {
-				final int columnIndex = c.getColumnIndex(IssueStatusesDbAdapter.KEY_IS_DEFAULT);
+				final int columnIndex = c.getColumnIndex(col);
 				if (col.equals(IssueStatusesDbAdapter.KEY_IS_DEFAULT)) {
-					is_default = Integer.parseInt(c.getString(columnIndex)) > 0;
+					is_default = c.getInt(columnIndex) > 0;
 				} else if (col.equals(IssueStatusesDbAdapter.KEY_IS_CLOSED)) {
-					is_closed = Integer.parseInt(c.getString(columnIndex)) > 0;
-				} else if (col.equals(IssueStatusesDbAdapter.KEY_ID)) {
-					id = c.getLong(columnIndex);
-				} else if (col.equals(IssueStatusesDbAdapter.KEY_NAME)) {
-					name = c.getString(columnIndex);
+					is_closed = c.getInt(columnIndex) > 0;
 				} else if (col.equals(IssueStatusesDbAdapter.KEY_SERVER_ID)) {
 				} else {
 					L.e("Unhandled column " + col + "!", null);
@@ -31,5 +29,10 @@ public class IssueStatus extends Reference {
 				L.e("Unable to get issue is default from cursor", e);
 			}
 		}
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ":" + getClass().getSimpleName() + " { is_default: " + is_default + ", is_closed: " + is_closed + " }";
 	}
 }
