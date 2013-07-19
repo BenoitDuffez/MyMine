3e48e7dc52462735bf9d3c3363b9555f904ed511
Benoit Duffez <benoit@bicou.net>
2013-07-19 14:43:10 +0200

    forgot to add files to previous commit

---
+++ src/net/bicou/redmine/app/drawers/MenuItemProject.java
@@ -0,0 +1,44 @@
+package net.bicou.redmine.app.drawers;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import net.bicou.redmine.R;
+
+/**
+ * Created by bicou on 19/07/13.
+ */
+public class MenuItemProject extends MenuItem {
+	String mText;
+
+	private class ViewHolder {
+		ImageView icon;
+		TextView text;
+	}
+
+	public MenuItemProject(DrawerMenuFragment drawerMenuFragment, String text) {
+		super(drawerMenuFragment);
+		mText = text;
+	}
+
+	@Override
+	public View fillView(View convertView, ViewGroup parent) {
+		View v;
+		ViewHolder holder;
+
+		if (convertView == null) {
+			v = drawerMenuFragment.getActivity().getLayoutInflater().inflate(R.layout.drawer_menu_item, parent, false);
+			holder = new ViewHolder();
+			holder.icon = (ImageView) v.findViewById(R.id.slidingmenu_item_icon);
+			holder.text = (TextView) v.findViewById(R.id.slidingmenu_item_text);
+			v.setTag(holder);
+		} else {
+			v = convertView;
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.text.setText(mText);
+
+		return v;
+	}
+}
---
+++ src/net/bicou/redmine/app/drawers/MenuItemQuery.java
@@ -0,0 +1,45 @@
+package net.bicou.redmine.app.drawers;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import net.bicou.redmine.R;
+
+/**
+ * Created by bicou on 19/07/13.
+ */
+public class MenuItemQuery extends MenuItem {
+	String server, query;
+
+	public MenuItemQuery(final DrawerMenuFragment drawerMenuFragment, String queryName, String serverName) {
+		super(drawerMenuFragment);
+		query = queryName;
+		server = serverName;
+	}
+
+	private static class ViewHolder {
+		TextView server, query;
+	}
+
+	@Override
+	public View fillView(View convertView, ViewGroup parent) {
+		View v;
+		ViewHolder holder;
+
+		if (convertView == null) {
+			v = drawerMenuFragment.getActivity().getLayoutInflater().inflate(R.layout.drawer_menu_issue_query, parent, false);
+			holder = new ViewHolder();
+			holder.server = (TextView) v.findViewById(R.id.drawer_query_name);
+			holder.query = (TextView) v.findViewById(R.id.drawer_query_server);
+			v.setTag(holder);
+		} else {
+			v = convertView;
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		holder.query.setText(query);
+		holder.server.setText(server);
+
+		return v;
+	}
+}
