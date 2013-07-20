4cedd0a2290fe5dd4e3aecca35261b5ac3d79422
Benoit Duffez <benoit@bicou.net>
2013-07-21 00:02:21 +0200

    added a way to target device/emulator for the db pull/push tool

--- tools/redminedb.sh
+++ tools/redminedb.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-adb shell su -c "cat /data/data/net.bicou.redmine/databases/redmine.db > /sdcard/redmine.db" && adb pull /sdcard/redmine.db .redmine.db && /opt/local/bin/sqlite3 -column -header .redmine.db
+adb $1 shell su -c "cat /data/data/net.bicou.redmine/databases/redmine.db > /sdcard/redmine.db" && adb $1 pull /sdcard/redmine.db .redmine.db && /opt/local/bin/sqlite3 -column -header .redmine.db
 
