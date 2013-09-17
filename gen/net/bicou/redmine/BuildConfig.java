7517495faaa135d664606f81683151b827c16259
Benoit Duffez <benoit@bicou.net>
2013-09-17 22:49:55 +0200

    updated gitignore

--- gen/net/bicou/redmine/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package net.bicou.redmine;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
