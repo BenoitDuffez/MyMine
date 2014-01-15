ae3658d97b7e8c539151c89bded4e0019c2d8b3e
Benoit Duffez <benoit@bicou.net>
2014-01-16 00:57:41 +0100

    fixed wrong getClass calls

--- MyMine/src/main/java/net/bicou/redmine/app/drawers/DrawerActivity.java
+++ MyMine/src/main/java/net/bicou/redmine/app/drawers/DrawerActivity.java
@@ -23,130 +23,130 @@ import net.bicou.redmine.util.L;
  * Created by bicou on 12/06/13.
  */
 public abstract class DrawerActivity extends ActionBarActivity {
-    DrawerLayout mDrawerLayout;
-    View mDrawerMenu;
-    private String mTitle, mTitleDrawer;
-    protected ActionBarDrawerToggle mDrawerToggle;
+	DrawerLayout mDrawerLayout;
+	View mDrawerMenu;
+	private String mTitle, mTitleDrawer;
+	protected ActionBarDrawerToggle mDrawerToggle;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        L.d("newConfig: " + newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		L.d("newConfig: " + newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 
-    /**
-     * Can be overriden by subclasses
-     */
-    protected Fragment getDrawerFragment() {
-        return new DrawerMenuFragment();
-    }
+	/**
+	 * Can be overriden by subclasses
+	 */
+	protected Fragment getDrawerFragment() {
+		return new DrawerMenuFragment();
+	}
 
-    int setContentViewCount = 0;
+	int setContentViewCount = 0;
 
-    @Override
-    public void setContentView(int layoutResId) {
-        setContentViewCount++;
-        if (setContentViewCount == 1) {
-            super.setContentView(layoutResId);
-        } else {
-            throw new IllegalStateException("Don't call setContentView from your activity");
-        }
-    }
+	@Override
+	public void setContentView(int layoutResId) {
+		setContentViewCount++;
+		if (setContentViewCount == 1) {
+			super.setContentView(layoutResId);
+		} else {
+			throw new IllegalStateException("Don't call setContentView from your activity");
+		}
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        supportRequestWindowFeature(Window.FEATURE_PROGRESS);
-        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setContentView(R.layout.activity_drawer);
-        setSupportProgressBarIndeterminate(true);
-        setSupportProgressBarIndeterminateVisibility(false);
+		supportRequestWindowFeature(Window.FEATURE_PROGRESS);
+		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setContentView(R.layout.activity_drawer);
+		setSupportProgressBarIndeterminate(true);
+		setSupportProgressBarIndeterminateVisibility(false);
 
 
-        mDrawerMenu = findViewById(R.id.navigation_drawer);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+		mDrawerMenu = findViewById(R.id.navigation_drawer);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
 
-        if (mDrawerMenu == null || mDrawerLayout == null) {
-            throw new IllegalStateException("a DrawerActivity must have a drawer layout id=main_drawer_layout and a drawer list id=android.R.id.list");
-        }
+		if (mDrawerMenu == null || mDrawerLayout == null) {
+			throw new IllegalStateException("a DrawerActivity must have a drawer layout id=main_drawer_layout and a drawer list id=android.R.id.list");
+		}
 
-        mTitle = getString(R.string.app_name);
-        mTitleDrawer = getString(R.string.drawer_title);
-    }
+		mTitle = getString(R.string.app_name);
+		mTitleDrawer = getString(R.string.drawer_title);
+	}
 
-    public void closeDrawer() {
-        mDrawerLayout.closeDrawer(mDrawerMenu);
-    }
+	public void closeDrawer() {
+		mDrawerLayout.closeDrawer(mDrawerMenu);
+	}
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-            public void onDrawerClosed(View view) {
-                getSupportActionBar().setTitle(mTitle);
-                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getSupportActionBar().setTitle(mTitle);
+				supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
 
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle(mTitleDrawer);
-                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                Fragment frag = getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-                if (frag instanceof DrawerMenuFragment) {
-                    ((DrawerMenuFragment) frag).refreshMenu();
-                }
-            }
-        };
+			public void onDrawerOpened(View drawerView) {
+				getSupportActionBar().setTitle(mTitleDrawer);
+				supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+				Fragment frag = getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+				if (frag instanceof DrawerMenuFragment) {
+					((DrawerMenuFragment) frag).refreshMenu();
+				}
+			}
+		};
 
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        ActionBar actionBar = getSupportActionBar();
+		ActionBar actionBar = getSupportActionBar();
 
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(!getClass().equals(MainActivity.class));
-        //actionBar.setDisplayUseLogoEnabled(!getClass().equals(MainActivity.class));
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setDisplayShowHomeEnabled(!((Object) this).getClass().equals(MainActivity.class));
+		//actionBar.setDisplayUseLogoEnabled(!getClass().equals(MainActivity.class));
 
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        L.d("item: " + item);
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                if (!this.getClass().equals(MainActivity.class)) {
-                    NavUtils.navigateUpFromSameTask(this);
-                    return true;
-                } else {
-                    if (mDrawerLayout.isDrawerOpen(mDrawerMenu)) {
-                        mDrawerLayout.closeDrawer(mDrawerMenu);
-                    } else {
-                        mDrawerLayout.openDrawer(mDrawerMenu);
-                    }
-                }
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		L.d("item: " + item);
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			if (!((Object) this).getClass().equals(MainActivity.class)) {
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			} else {
+				if (mDrawerLayout.isDrawerOpen(mDrawerMenu)) {
+					mDrawerLayout.closeDrawer(mDrawerMenu);
+				} else {
+					mDrawerLayout.openDrawer(mDrawerMenu);
+				}
+			}
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EasyTracker.getInstance(this).activityStart(this);
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this);
+	}
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        EasyTracker.getInstance(this).activityStop(this);
-    }
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this);
+	}
 }
